287a454b9240cbdd2f4a57684a477ce370e97acd
xvertile <123594600+xvertile@users.noreply.github.com>
2024-02-20 21:25:15 +0100

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -6,12 +6,25 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# Download the configuration file and place it in /etc/sysctl.d/
+wget -O /etc/sysctl.d/99-custom.conf https://raw.githubusercontent.com/xvertile/proxy-server-setup/main/99-custom.conf
+
+# Reload system-wide sysctl settings
+sudo sysctl --system
+
 # Set ulimit to the maximum possible value and make it persistent
 # Get the maximum allowed number of open file descriptors
 MAX_ULIMIT=$(ulimit -Hn)
 # Make the ulimit setting persistent across reboots by adding it to /etc/security/limits.conf
 echo "* soft nofile $MAX_ULIMIT" >> /etc/security/limits.conf
 echo "* hard nofile $MAX_ULIMIT" >> /etc/security/limits.conf
+
+# Update package lists
+sudo apt-get update
+
+# Install iptables-persistent to save iptables rules across reboots
+# The DEBIAN_FRONTEND=noninteractive flag helps in avoiding interactive prompts
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent
 
 # Block all mail ports from any connection anywhere
 # Define common mail ports
@@ -24,8 +37,5 @@ done
 
 # Save iptables rules to make them persistent across reboots
 iptables-save > /etc/iptables/rules.v4
-wget -O /etc/sysctl.d/99-custom.conf https://raw.githubusercontent.com/xvertile/proxy-server-setup/main/99-custom.conf
 
-# Reload system-wide sysctl settings
-sudo sysctl --system
 echo "Configuration complete. System modifications applied."
